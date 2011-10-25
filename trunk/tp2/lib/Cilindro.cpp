509353ef2bf7e81741bb122c828177a07ebf4d06
gygounet@gmail.com <gygounet@gmail.com@57eeccb7-2dbe-dfe9-f1ce-6dd61f39e22f>
2011-10-25 14:35:07 +0000

    dibujado de cuerpos

--- trunk/tp2/lib/Cilindro.cpp
+++ trunk/tp2/lib/Cilindro.cpp
@@ -5,8 +5,10 @@
 Cilindro::Cilindro(){
 }
 
-void Cilindro::dibujarCirculo(int segmentos, float z){
+void Cilindro::dibujarCirculo(int segmentos, float radio, float z){
 
+	float inc = 0.1;
+	float incremento = inc;
     glBegin(GL_TRIANGLE_FAN);
     if(z>0)
     	glNormal3f(0, 0, 1);
@@ -15,43 +17,46 @@ void Cilindro::dibujarCirculo(int segmentos, float z){
     glTexCoord2f(0.5,0.5);
     glVertex3f(0, 0, z);
     for(float i = 0; i <= 2*M_PI; i += 2*M_PI / segmentos) {
-    	glTexCoord2f(1-(cos(i) + 1)/2, (sin(i) + 1 )/2);
-    	glVertex3f(cos(i), sin(i), z);
+    	glTexCoord2f(0.5 + cos(i)*radio*incremento, 0.5 + sin(i)*radio*incremento);//ojo normalizar para que radio pueda ser distinto de 0.5
+    	glVertex3f(cos(i)*radio*incremento, sin(i)*radio*incremento, z);
     }
     glEnd();
+
+	glBegin(GL_TRIANGLE_STRIP);//ojo normal
+	for (; incremento < 1; incremento += inc)
+		for (float i = 0; i <= 2 * M_PI; i += 2 * M_PI / segmentos) {
+			float _cos = cos(i)*radio;
+			float _sin = sin(i)*radio;
+		   	glTexCoord2f(0.5 + _cos*incremento, 0.5 + _sin*incremento);//ojo normalizar para que radio pueda ser distinto de 0.5
+		    glVertex3f(_cos*incremento, _sin*incremento, z);
+		   	glTexCoord2f(0.5 + _cos*(incremento+inc), 0.5 + _sin*(incremento+inc));//ojo normalizar para que radio pueda ser distinto de 0.5
+		    glVertex3f(_cos*(incremento+inc),_sin*(incremento+inc), z);
+		}
+	glEnd();
 }
 
 void Cilindro::do_dibujar() {
-	int segmentos = 30;
+	int segmentos = 42;
 	glPushMatrix();
 	glRotatef(60,0,1,1);
-	glScalef(0.4,0.4,1);
 	glBegin(GL_TRIANGLE_STRIP);
+	float radio = 0.5;
 	float inc = 0.1;
-	for(float incremento = 0; incremento < 1; incremento += inc)
-	for(float i = 0; i <= 2*M_PI; i += 2*M_PI / segmentos){
-	    float _cos = cos(i);
-	    float _sin = sin(i);
-	    glNormal3f(_cos, _sin, 0);
-	    glTexCoord2f(1 - i/(2*M_PI),incremento);
-	    glVertex3f(_cos, _sin, -0.5 + incremento);
-	    glTexCoord2f(1 - i/(2*M_PI), inc + incremento);
-	    glVertex3f(_cos, _sin, -0.5 + inc + incremento);
-	}
+	for (float incremento = 0; incremento < 1; incremento += inc)
+		for (float i = 0; i <= 2 * M_PI; i += 2 * M_PI / segmentos) {
+			float _cos = cos(i)*radio;
+			float _sin = sin(i)*radio;
+			glNormal3f(_cos, _sin, 0);
+			glTexCoord2f(1 - i / (2 * M_PI), incremento);
+			glVertex3f(_cos, _sin, -0.5 + incremento);
+			glTexCoord2f(1 - i / (2 * M_PI), inc + incremento);
+			glVertex3f(_cos, _sin, -0.5 + inc + incremento);
+		}
 	glEnd();
 
 	//Dibujo las tapas
-	glPushMatrix();
-//	glTranslatef(0, 0, 0.5);
-	glScalef(1, 1, 1);
-	dibujarCirculo(segmentos, 0.5);
-	glPopMatrix();
-
-	glPushMatrix();
-//	glTranslatef(0, 0, -0.5);
-//	glRotatef(180, 1, 0, 0);
-	dibujarCirculo(segmentos,-0.5);
-	glPopMatrix();
+	dibujarCirculo(segmentos, radio, 0.5);
+	dibujarCirculo(segmentos, radio, -0.5);
 	glPopMatrix();
 }
 
--- trunk/tp2/lib/Cubo.cpp
+++ trunk/tp2/lib/Cubo.cpp
@@ -8,24 +8,20 @@ void Cubo::cuadradoMedio(){
   glBegin(GL_QUADS);
     glNormal3f(0,0,1);
     float incremento = 0.05;
-    for(float incy=0; incy < 1; incy += incremento){
-    	for(float incx=0; incx < 1; incx += incremento){
-    		glTexCoord2f(1 - incy, 1 - incx);
-			glVertex3f(-0.5 + incx, -0.5 + incy,0.5);
+	for (float incy = 0; incy < 1; incy += incremento)
+		for (float incx = 0; incx < 1; incx += incremento) {
+			glTexCoord2f(1 - incy, 1 - incx);
+			glVertex3f(-0.5 + incx, -0.5 + incy, 0.5);
 			glTexCoord2f(1 - incremento - incy, 1 - incx);
-			glVertex3f(-0.5 + incx, -0.5 + incremento + incy,0.5);
+			glVertex3f(-0.5 + incx, -0.5 + incremento + incy, 0.5);
 			glTexCoord2f(1 - incremento - incy, 1 - incremento - incx);
-			glVertex3f(-0.5 + incremento + incx, -0.5 + incremento + incy,0.5);
+			glVertex3f(-0.5 + incremento + incx, -0.5 + incremento + incy, 0.5);
 			glTexCoord2f(1 - incy, 1 - incremento - incx);
-			glVertex3f(-0.5 + incremento + incx, -0.5 + incy,0.5);
-    	}
-    }
- glEnd();
+			glVertex3f(-0.5 + incremento + incx, -0.5 + incy, 0.5);
+		}
 
- glBegin(GL_QUADS);
    glNormal3f(0,0,-1);
-  // float incremento = 0.1;
-   for(float incy=0; incy < 1; incy += incremento){
+   for(float incy=0; incy < 1; incy += incremento)
    	for(float incx=0; incx < 1; incx += incremento){
    		glTexCoord2f(1 - incy, 1 - incx);
 			glVertex3f(-0.5 + incx, -0.5 + incy,-0.5);
@@ -36,13 +32,9 @@ void Cubo::cuadradoMedio(){
 			glTexCoord2f(1 - incy, 1 - incremento - incx);
 			glVertex3f(-0.5 + incremento + incx, -0.5 + incy,-0.5);
    	}
-   }
-glEnd();
 
-glBegin(GL_QUADS);
   glNormal3f(-1,0,0);
- // float incremento = 0.1;
-  for(float incy=0; incy < 1; incy += incremento){
+  for(float incy=0; incy < 1; incy += incremento)
   	for(float incx=0; incx < 1; incx += incremento){
   		glTexCoord2f(1 - incy, 1 - incx);
 			glVertex3f(-0.5,-0.5 + incx, -0.5 + incy);
@@ -53,13 +45,9 @@ glBegin(GL_QUADS);
 			glTexCoord2f(1 - incy, 1 - incremento - incx);
 			glVertex3f(-0.5,-0.5 + incremento + incx, -0.5 + incy);
   	}
-  }
-glEnd();
 
-glBegin(GL_QUADS);
   glNormal3f(1,0,0);
- // float incremento = 0.1;
-  for(float incy=0; incy < 1; incy += incremento){
+  for(float incy=0; incy < 1; incy += incremento)
   	for(float incx=0; incx < 1; incx += incremento){
   		glTexCoord2f(1 - incy, 1 - incx);
 			glVertex3f(0.5,-0.5 + incx, -0.5 + incy);
@@ -70,13 +58,9 @@ glBegin(GL_QUADS);
 			glTexCoord2f(1 - incy, 1 - incremento - incx);
 			glVertex3f(0.5,-0.5 + incremento + incx, -0.5 + incy);
   	}
-  }
-glEnd();
 
-glBegin(GL_QUADS);
   glNormal3f(0,-1,0);
- // float incremento = 0.1;
-  for(float incy=0; incy < 1; incy += incremento){
+  for(float incy=0; incy < 1; incy += incremento)
   	for(float incx=0; incx < 1; incx += incremento){
   		glTexCoord2f(1 - incy, 1 - incx);
 			glVertex3f(-0.5 + incx,-0.5, -0.5 + incy);
@@ -87,13 +71,9 @@ glBegin(GL_QUADS);
 			glTexCoord2f(1 - incy, 1 - incremento - incx);
 			glVertex3f(-0.5 + incremento + incx,-0.5, -0.5 + incy);
   	}
-  }
-glEnd();
 
-glBegin(GL_QUADS);
   glNormal3f(0,1,0);
- // float incremento = 0.1;
-  for(float incy=0; incy < 1; incy += incremento){
+  for(float incy=0; incy < 1; incy += incremento)
   	for(float incx=0; incx < 1; incx += incremento){
   		glTexCoord2f(1 - incy, 1 - incx);
 			glVertex3f(-0.5 + incx,0.5, -0.5 + incy);
@@ -104,38 +84,16 @@ glBegin(GL_QUADS);
 			glTexCoord2f(1 - incy, 1 - incremento - incx);
 			glVertex3f(-0.5 + incremento + incx,0.5, -0.5 + incy);
   	}
-  }
-glEnd();
+  	glEnd();
 }
-
+//glNormal3f(normalx,normaly,normalz);
+// float incremento = 0.05;
+// for (float incz = 0; incz < 1 && normalz == 0; incz += incremento*normalz)
+//	for (float incy = 0; incy < 1; incy += incremento)
+//		for (float incx = 0; incx < 1; incx += incremento) {
+//			glTexCoord2f(1 - incy, 1 - incx);
+//			glVertex3f((-0.5 + incx)*(1 - abs(normalx)) + 0.5*normalx, (-0.5 + incy)*(1 - abs(normaly)) + 0.5*normaly, (-0.5 + incz)*(1 - abs(normalz)) + 0.5*normalz);
 
 void Cubo::do_dibujar() {
 	cuadradoMedio();
-/*
-	glPushMatrix();
-	glScalef(0.8,0.8,0.8);
-	glRotatef(45,0,1,1);
-	glBindTexture(GL_TEXTURE_2D, texture);
-	float angulos[4] = { 0, 90, 180, 270 };
-	for (int i = 0; i < 4; i++) {
-		glPushMatrix();
-		glRotatef(angulos[i], 1, 0, 0);
-		glTranslatef(0, 0, 0.5);
-		cuadradoMedio();
-		glPopMatrix();
-	}
-  
-  glPushMatrix();
-    glRotatef(90, 0, 1, 0);
-    glTranslatef(0,0,0.5);
-    cuadradoMedio();
-  glPopMatrix();
-  
-  glPushMatrix();
-    glRotatef(-90, 0, 1, 0);
-    glTranslatef(0,0,0.5);
-    cuadradoMedio();
-  glPopMatrix();
-  glPopMatrix();
-  */
 }
